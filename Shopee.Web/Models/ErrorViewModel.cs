eab557cee347b7cb093f5b97fefe160e3ec91548
Axmadjonov Abdulloh <abdulloh.7013@gmail.com>
2023-04-07 22:50:14 +0500

    Initial commit

---
+++ Shopee.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Shopee.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

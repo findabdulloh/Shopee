f160f7e6506847eea0dd51c57a1a1b5d9a5e4548
KomronSharpist <komrondeveloper@gmail.com>
2023-04-10 13:06:27 +0500

    Commit

--- Shopee.Service/Services/CartService.cs
+++ Shopee.Service/Services/CartService.cs
@@ -143,7 +143,7 @@ public class CartService : ICartService
     {
         var user = await userRepo.GetAsync(u => u.Id == userId);
 
-        var entity = await cartRepo.GetAsync(o => o.Id == user.CartId);
+        var entity = await cartRepo.GetAsync(o => o.Id == user.Id);
         if (entity is null) return null;
 
         var cartItems = await orderItemService
